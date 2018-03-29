e0bee77f083b2f96bdf46a099a3d012875bf614a
jinchung <jin.chung17@gmail.com>
2018-03-28 23:27:57 -0500

    separated out push notifications to separate architecture

--- wallet_connect/__init__.py
+++ wallet_connect/__init__.py
@@ -8,14 +8,13 @@ from aiohttp import web
 import boto3
 
 import wallet_connect.keystore
-from wallet_connect.push_notifications import PushNotificationsService
 from wallet_connect.errors import KeystoreWriteError, KeystoreFetchError, FirebaseError, KeystoreTokenExpiredError, InvalidApiKey, KeystoreFcmTokenError
 
 routes = web.RouteTableDef()
 
 API='io.wallet.connect.api_gateway'
 REDIS='io.wallet.connect.redis'
-PUSH='io.wallet.connect.push_notifications'
+SESSION='io.wallet.connect.session'
 KEY='key'
 LOCAL='local'
 SERVICE='service'
@@ -52,7 +51,7 @@ async def request_device_details(request):
     redis_conn = get_redis_master(request.app)
     await keystore.add_request_for_device_details(redis_conn, session_token)
     session_data = {"sessionToken": session_token}
-    return web.json_response(session_data, status=201)
+    return web.json_response(session_data)
   except KeyError:
     return web.json_response(error_message("Incorrect input parameters"), status=400)
   except TypeError:
@@ -70,11 +69,12 @@ async def update_device_details(request):
   request_json = await request.json()
   try:
     session_token = request_json['sessionToken']
-    fcm_token = request_json['fcmToken']
+    fcm_key = request_json['fcmKey']
+    wallet_webhook = request_json['walletWebhook']
     encrypted_payload = request_json['encryptedPayload']
     redis_conn = get_redis_master(request.app)
     device_uuid = str(uuid.uuid4())
-    await keystore.add_device_fcm_token(redis_conn, device_uuid, fcm_token)
+    await keystore.add_device_fcm_data(redis_conn, device_uuid, wallet_webhook, fcm_key)
     await keystore.update_device_details(redis_conn, device_uuid, session_token, encrypted_payload)
     device_uuid_data = {"deviceUuid": device_uuid}
     return web.json_response(device_uuid_data, status=202)
@@ -118,20 +118,17 @@ async def add_transaction_details(request):
     transaction_uuid = str(uuid.uuid4())
     device_uuid = request_json['deviceUuid']
     encrypted_payload = request_json['encryptedPayload']
+    # TODO could be optional notification details
     notification_title = request_json['notificationTitle']
     notification_body = request_json['notificationBody']
     redis_conn = get_redis_master(request.app)
     await keystore.add_transaction_details(redis_conn, transaction_uuid, device_uuid, encrypted_payload)
 
-    # Send push notification
-    fcm_token = await keystore.get_device_fcm_token(redis_conn, device_uuid)
-    push_notifications_service = request.app[PUSH][SERVICE]
-    data_message = {"deviceUuid": device_uuid, "transactionUuid": transaction_uuid }
-    await push_notifications_service.notify_single_device(
-        registration_id=fcm_token,
-        message_title=notification_title,
-        message_body=notification_body,
-        data_message=data_message)
+    # Notify wallet webhook
+    fcm_data = await keystore.get_device_fcm_data(redis_conn, device_uuid)
+    session = request.app[SESSION]
+    await send_webhook_request(session, fcm_data, transaction_uuid, notification_title, notification_body)
+    data_message = {"transactionUuid": transaction_uuid}
     return web.json_response(data_message, status=201)
   except KeyError:
     return web.json_response(error_message("Incorrect input parameters"), status=400)
@@ -173,9 +170,11 @@ async def update_transaction_status(request):
     request_json = await request.json()
     transaction_uuid = request_json['transactionUuid']
     device_uuid = request_json['deviceUuid']
-    encrypted_payload = request_json['encryptedPayload']
+    transaction_status = request_json['transactionStatus']
+    transaction_hash = request_json['transactionHash'] if ('transactionHash' in request_json) else None
     redis_conn = get_redis_master(request.app)
-    await keystore.update_transaction_status(redis_conn, transaction_uuid, device_uuid, encrypted_payload)
+    await keystore.update_transaction_status(redis_conn, transaction_uuid, device_uuid,
+                                             transaction_status, transaction_hash)
     return web.Response(status=201)
   except KeyError:
     return web.json_response(error_message("Incorrect input parameters"), status=400)
@@ -189,18 +188,17 @@ async def update_transaction_status(request):
       return web.json_response(error_message("Error unknown"), status=500)
 
 
-@routes.post('/get-transaction-hash')
-async def get_transaction_hash(request):
+@routes.post('/get-transaction-status')
+async def get_transaction_status(request):
   try:
     await check_authorization(request)
     request_json = await request.json()
     transaction_uuid = request_json['transactionUuid']
     device_uuid = request_json['deviceUuid']
     redis_conn = get_redis_master(request.app)
-    transaction_hash = await keystore.get_transaction_hash(redis_conn, transaction_uuid, device_uuid)
-    if transaction_hash:
-      json_response = {"transactionHash": transaction_hash}
-      return web.json_response(json_response)
+    transaction_status = await keystore.get_transaction_status(redis_conn, transaction_uuid, device_uuid)
+    if transaction_status:
+      return web.json_response(transaction_status)
     else:
       return web.Response(status=204)
   except KeyError:
@@ -213,20 +211,30 @@ async def get_transaction_hash(request):
     return web.json_response(error_message("Error unknown"), status=500)
 
 
+async def send_webhook_request(session, fcm_data, transaction_uuid, notification_title,
+                               notification_body):
+  fcm_key = fcm_data['fcm_key']
+  wallet_webhook = fcm_data['wallet_webhook']
+  payload = {
+    'transactionUuid': transaction_uuid,
+    'fcmKey': fcm_key,
+    'notificationTitle': notification_title,
+    'notificationBody': notification_body
+  }
+  headers = {'Content-Type': 'application/json'}
+  response = await session.post(wallet_webhook, json=payload, headers=headers)
+  if response.status != 200:
+    raise FirebaseError("Wallet webhook error")
+
+
 def get_kms_parameter(param_name):
   ssm = boto3.client('ssm', region_name='us-east-2')
   response = ssm.get_parameters(Names=[param_name], WithDecryption=True)
   return response['Parameters'][0]['Value']
 
 
-async def initialize_push_notifications(app):
-  local = app[PUSH][LOCAL]
-  if local:
-    app[PUSH][SERVICE] = PushNotificationsService(debug=local)
-  else:
-    api_key = get_kms_parameter('fcm-server-key')
-    session = aiohttp.ClientSession(loop=app.loop)
-    app[PUSH][SERVICE] = PushNotificationsService(session=session, api_key=api_key)
+async def initialize_client_session(app):
+  app[SESSION] = aiohttp.ClientSession(loop=app.loop)
 
 
 async def initialize_keystore(app):
@@ -250,30 +258,27 @@ async def close_keystore(app):
   await app[REDIS][SERVICE].wait_closed()
 
 
-async def close_push_notification_connection(app):
-  if app[PUSH][SERVICE].session:
-    await app[PUSH][SERVICE].session.close()
+async def close_client_session_connection(app):
+  await app[SESSION].close()
 
 
 def main(): 
   parser = argparse.ArgumentParser()
   parser.add_argument('--redis-local', action='store_true')
-  parser.add_argument('--push-local', action='store_true')
   parser.add_argument('--api-local', action='store_true')
   args = parser.parse_args()
 
   app = web.Application()
   app[API] = {LOCAL: args.api_local}
   app[REDIS] = {LOCAL: args.redis_local}
-  app[PUSH] = {LOCAL: args.push_local}
-  app.on_startup.append(initialize_push_notifications)
+  app.on_startup.append(initialize_client_session)
   app.on_startup.append(initialize_keystore)
   app.on_startup.append(initialize_api_gateway)
   app.on_cleanup.append(close_keystore)
-  app.on_cleanup.append(close_push_notification_connection)
+  app.on_cleanup.append(close_client_session_connection)
   app.router.add_routes(routes)
   asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
-  web.run_app(app)
+  web.run_app(app, port=8081)
 
 
 if __name__ == '__main__':
--- wallet_connect/keystore.py
+++ wallet_connect/keystore.py
@@ -20,7 +20,7 @@ async def create_sentinel_connection(event_loop, sentinels):
 
 async def add_request_for_device_details(conn, session_token):
   key = session_key(session_token)
-  success = await write(conn, key, '', expiration_in_seconds=5*60)
+  success = await write(conn, key, '')
   if not success:
     raise KeystoreWriteError('Error adding request for details')
 
@@ -34,19 +34,22 @@ async def update_device_details(conn, device_uuid, session_token, encrypted_payl
     raise KeystoreTokenExpiredError
 
 
-async def add_device_fcm_token(conn, device_uuid, fcm_token):
+async def add_device_fcm_data(conn, device_uuid, wallet_webhook, fcm_key):
+  # TODO what if we want wallet_webhook to be null?
   key = device_uuid_key(device_uuid)
-  success = await write(conn, key, fcm_token, expiration_in_seconds=30*60)
+  data = {'fcm_key': fcm_key, 'wallet_webhook': wallet_webhook}
+  fcm_data = json.dumps(data)
+  success = await write(conn, key, fcm_data, expiration_in_seconds=24*60*60)
   if not success:
-    raise KeystoreWriteError("Could not write device FCM token")
+    raise KeystoreWriteError("Could not write device FCM data")
   
 
-async def get_device_fcm_token(conn, device_uuid):
+async def get_device_fcm_data(conn, device_uuid):
   device_key = device_uuid_key(device_uuid)
-  fcm_token = await conn.get(device_key)
-  if not fcm_token:
+  data = await conn.get(device_key)
+  if not data:
     raise KeystoreFcmTokenError
-  return fcm_token
+  return json.loads(data)
 
 
 async def get_device_details(conn, session_token):
@@ -60,7 +63,8 @@ async def get_device_details(conn, session_token):
 
 async def add_transaction_details(conn, transaction_uuid, device_uuid, encrypted_payload):
   key = transaction_key(transaction_uuid, device_uuid)
-  success = await write(conn, key, encrypted_payload, expiration_in_seconds=300)
+  # TODO how long should this be here for?
+  success = await write(conn, key, encrypted_payload, expiration_in_seconds=60*60)
   if not success:
     raise KeystoreWriteError("Error adding transaction details")
 
@@ -75,19 +79,18 @@ async def get_transaction_details(conn, transaction_uuid, device_uuid):
     return details
 
 
-async def update_transaction_status(conn, transaction_uuid, device_uuid, encrypted_payload):
+async def update_transaction_status(conn, transaction_uuid, device_uuid, transaction_status, transaction_hash):
   key = transaction_hash_key(transaction_uuid, device_uuid)
-  success = await write(conn, key, encrypted_payload)
-  if not success:
-    raise KeystoreWriteError("Error writing transaction hash")
+  data = {'transaction_status': transaction_status, 'transaction_hash': transaction_hash}
+  status_details = json.dumps(data)
+  success = await write(conn, key, status_details)
 
 
-async def get_transaction_hash(conn, transaction_uuid, device_uuid):
+async def get_transaction_status(conn, transaction_uuid, device_uuid):
   key = transaction_hash_key(transaction_uuid, device_uuid)
-  transaction_hash = await conn.get(key)
-  if transaction_hash:
-    await conn.delete(key)
-  return transaction_hash
+  details = await conn.get(key)
+  transaction_status = json.loads(details)
+  return transaction_status
 
 
 def api_redis_key(api_key):
@@ -110,7 +113,7 @@ def transaction_hash_key(transaction_uuid, device_uuid):
   return "txnhash:{}:{}".format(transaction_uuid, device_uuid)
 
 
-async def write(conn, key, value='', expiration_in_seconds=120, write_only_if_exists=False):
+async def write(conn, key, value='', expiration_in_seconds=5*60, write_only_if_exists=False):
   exist = 'SET_IF_EXIST' if write_only_if_exists else None
   success = await conn.set(key, value, expire=expiration_in_seconds, exist=exist)
   return success
