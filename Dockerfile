ec52f05580be9b093e9921534defdaf8f8e89978
Ashutosh Dwivedi <dashu2410@gmail.com>
2018-06-30 17:27:18 +0530

    make docker based server run default to HTTPS

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,7 @@ RUN apt install -y python3-pip git redis-server nginx
 RUN git clone https://github.com/WalletConnect/py-walletconnect-bridge
 WORKDIR /py-walletconnect-bridge
 RUN pip3 install -r requirements.txt
-RUN python3 setup.py develop
+RUN python3 setup.py install
 COPY docker-entrypoint.sh /bin/
 RUN chmod +x /bin/docker-entrypoint.sh
 ENTRYPOINT ["/bin/docker-entrypoint.sh"]
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,7 +7,11 @@ echo "started redis server"
 sleep 5
 walletconnect-bridge --redis-local --port 8080 --host 0.0.0.0 &
 echo "started wallet connect server"
- service nginx start
+echo "generating keys"
+mkdir /keys
+openssl req -x509 -newkey rsa:4096 -keyout /keys/key.pem -out /keys/cert.pem -days 365 -nodes -subj "/C=US/ST=Oregon/L=Portland/O=Company Name/OU=Org/CN=bridge.mydomain.com"
+echo "generated keys"
+service nginx start
 echo "started nginx service"
 #now sleeping infinitely
 tail -f /dev/null
