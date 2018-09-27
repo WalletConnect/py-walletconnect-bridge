b1b513ebae9833794aac032275cec41f1f898a16
Pedro Gomes <pedrogomes94@gmail.com>
2018-09-27 13:14:10 -0700

    tweak

--- walletconnect_bridge/time.py
+++ walletconnect_bridge/time.py
@@ -5,5 +5,5 @@ def now():
   return now
 
 def get_expiration_time(ttl_in_seconds):
-  expires = int(time.time() + ttl_in_seconds) * 1000
+  expires = int(now() + ttl_in_seconds) * 1000
   return expires
